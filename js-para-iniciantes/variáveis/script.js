e85ae4ccc9fada3db5465c6e008f320f757af7d6
Francisco Araújo <fhydglan@gmail.com>
2023-07-24 13:22:57 -0300

    created new files

---
+++ js-para-iniciantes/variáveis/script.js
@@ -0,0 +1,26 @@
+var nome = "Francisco";
+let idade = 27;
+const possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Araujo",
+  cidade = "Fortaleza";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini);
+
+// const time = "Vasco";
+// time = "Flamengo";
+
+// console.log(time);
