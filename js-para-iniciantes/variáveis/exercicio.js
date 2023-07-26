b8d09668814673453af3195694683cc33904dbc6
Francisco Araújo <fhydglan@gmail.com>
2023-07-26 16:07:37 -0300

    Create exercicio.js

---
+++ js-para-iniciantes/variáveis/exercicio.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com o seu nome
+var nome = "Francisco";
+
+// Declarar uma variável com a sua idade
+var idade = 27;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Torta de frango";
+
+// Declarar 5 variáveis diferentes sem valores
+var sexo = "M",
+  cidade,
+  telefone,
+  corDosOlhos,
+  carroDosSonhos;
