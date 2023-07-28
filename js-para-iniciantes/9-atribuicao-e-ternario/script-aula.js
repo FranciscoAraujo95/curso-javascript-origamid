5e7851d49cf0502ef6e23cbdc6bf27c654444dbd
Francisco Araújo <fhydglan@gmail.com>
2023-07-28 16:46:23 -0300

    10

---
+++ js-para-iniciantes/9-atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,9 @@
+var numero = 20;
+var numero2 = 10; // numero = numero + 10
+numero += numero2;
+console.log(numero);
+console.log(numero2);
+
+var idade = 16;
+var podeBeber = idade >= 18 ? "Pode beber" : "Não pode beber";
+console.log(podeBeber);
