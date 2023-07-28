d5c9a4db9779c5d76b83be4c00b501d43ac9d585
Francisco Araújo <fhydglan@gmail.com>
2023-07-28 17:15:52 -0300

    Some changes

--- js-para-iniciantes/9-atribuicao-e-ternario/script-aula.js
+++ js-para-iniciantes/9-atribuicao-e-ternario/script-aula.js
@@ -4,6 +4,11 @@ numero += numero2;
 console.log(numero);
 console.log(numero2);
 
-var idade = 16;
+var idade = 27;
 var podeBeber = idade >= 18 ? "Pode beber" : "Não pode beber";
 console.log(podeBeber);
+
+var possuiFaculdade = false;
+//Se for retornar somente uma opção no if, não é necessário abrir parênteses, economizando código
+if (possuiFaculdade) console.log("Sim, possui");
+else console.log("Não possui");
