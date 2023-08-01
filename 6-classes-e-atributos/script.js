221b7debee3d34fe29f1d201afd8e15601df57b3
Francisco Araújo <fhydglan@gmail.com>
2023-08-01 10:09:43 -0300

    Update script.js

--- 6-classes-e-atributos/script.js
+++ 6-classes-e-atributos/script.js
@@ -1,17 +1,19 @@
 const menu = document.querySelector(".menu");
-menu.classList.add("ativo");
-menu.classList.remove("azul");
-menu.classList.toggle("azul");
+menu.classList.add("ativo"); //Adiciona a classe ativo
+menu.classList.remove("azul"); //Remove a classe azul
+menu.classList.toggle("azul"); //Se não tiver a classe, ele coloca, se tiver, ele retira
 
+//contains verifica se o elemento contém determinada classe
 if (menu.classList.contains("azul")) {
   menu.classList.add("possui-azul");
 } else {
   menu.classList.add("nao-possui-azul");
 }
 
+//Também dá pra adicionar classes utilizando o .className dessa forma
 menu.className += " vermelho";
 
 // console.log(menu.classList);
 
 const animais = document.querySelector(".animais");
-console.log(animais.attributes);
+console.log(animais.classList);
