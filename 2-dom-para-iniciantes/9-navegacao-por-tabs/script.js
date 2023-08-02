937271aac8f1dcbe2c4429ddd4f9209992798ad9
Francisco Araújo <fhydglan@gmail.com>
2023-08-02 18:36:20 -0300

    some files

--- 2-dom-para-iniciantes/9-navegacao-por-tabs/script.js
+++ 2-dom-para-iniciantes/9-navegacao-por-tabs/script.js
@@ -2,19 +2,23 @@
 const tabMenu = document.querySelectorAll(".js-tabmenu li");
 const tabContent = document.querySelectorAll(".js-tabcontent section");
 //deixa o primeiro item do content ativo ao carregar a página
-tabContent[0].classList.add("ativo");
 
-//Essa função vai fazer um loop por cada item das sections, removendo a classe ativo e adicionando apenas no elemento alvo
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove("ativo");
+//Esse if verificar se esses elementos existe, na página, é uma verificação de dois fatores, os dois tem que retornar true pro código executar
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
+
+  //Essa função vai fazer um loop por cada item das sections, removendo a classe ativo e adicionando apenas no elemento alvo
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  //Essa função faz um loop por todos os itens do animais-lista, adicionando um evento de click, e ao clique, passando a função de activeTab das sections, já passando o index de cada um, como foi definido no parâmetro "index" da função
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
   });
-  tabContent[index].classList.add("ativo");
 }
-
-//Essa função faz um loop por todos os itens do animais-lista, adicionando um evento de click, e ao clique, passando a função de activeTab das sections, já passando o index de cada um, como foi definido no parâmetro "index" da função
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener("click", () => {
-    activeTab(index);
-  });
-});
