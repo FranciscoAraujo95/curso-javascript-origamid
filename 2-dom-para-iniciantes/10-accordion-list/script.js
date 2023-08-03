795a2ae19186e78320ff70ef6d461a3f4ecc5fc8
Francisco Araújo <fhydglan@gmail.com>
2023-08-02 21:28:30 -0300

    changes

---
+++ 2-dom-para-iniciantes/10-accordion-list/script.js
@@ -0,0 +1,48 @@
+function initTabNav() {
+  //selecionando as imagens da ul de animais, puxando todas as li's
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  //deixa o primeiro item do content ativo ao carregar a página
+
+  //Esse if verifica se esses elementos existem na página, é uma verificação d dois fatores, os dois tem que retornar true pro código executar
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    //Essa função vai fazer um loop por cada item das sections, removendo a classe ativo e adicionando apenas no elemento alvo
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    //Essa função faz um loop por todos os itens do animais-lista, adicionando um evento de click, e ao clique, passando a função de activeTab das sections, já passando o index de cada um, como foi definido no parâmetro "index" da função
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
